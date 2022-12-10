b87b4ce4731e25f39318edad058fa46d6df72541
adelfani <adelfani804@gmail.com>
2022-12-10 11:23:04 +0100

    Add fun

--- js/components/Assignment.js
+++ js/components/Assignment.js
@@ -1,12 +1,13 @@
 export default {
   template: `
     <li>
-        <label>
+        <label class="p-2 flex justify-between item-center">
           {{ assignment.name }}
           <input
             type="checkbox"
             v-model="assignment.complete"
             :key="assignment.id"
+            class="ml-4"
           />
         </label>
     </li>
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -5,9 +5,17 @@ export default {
     AssignmentList,
   },
   template: `
-    <assignment-list :assignments="inProgressAssignments" title="In progress"></assignment-list>
-    <br />
-    <assignment-list :assignments="computedAssignments" title="Completed"></assignment-list>
+    <section class="space-y-6">
+        <assignment-list :assignments="inProgressAssignments" title="In progress"></assignment-list>
+        <assignment-list :assignments="computedAssignments" title="Completed"></assignment-list>
+
+        <form @submit.prevent="add">
+            <div class="border border-gray-600 text-black" >
+                <input v-model="newAssignment" placeholder="New assignment..." class="p-2" />
+                <button type="submit" class="bg-white p-2 border-l" >Add</button>
+            </div>
+        </form>
+    </section>
     `,
   data() {
     return {
@@ -16,6 +24,7 @@ export default {
         { name: "Assignment 2", complete: false, id: 2 },
         { name: "Assignment 3", complete: false, id: 3 },
       ],
+      newAssignment: "",
     };
   },
 
@@ -25,6 +34,17 @@ export default {
     },
     inProgressAssignments() {
       return this.assignments.filter((assignment) => !assignment.complete);
+    },
+  },
+  methods: {
+    add() {
+      this.assignments.push({
+        name: this.newAssignment,
+        complete: false,
+        id: this.assignments.length + 1,
+      });
+
+      this.newAssignment = "";
     },
   },
 };
