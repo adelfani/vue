92ca4a0b96f5ba2f225777252c3816984fdb06a4
adelfani <adelfani804@gmail.com>
2022-12-11 08:48:21 +0100

    final

--- project1/js/components/AssignmentCreate.js
+++ project1/js/components/AssignmentCreate.js
@@ -2,7 +2,7 @@ export default {
   template: `
     <form @submit.prevent="add">
         <div class="border border-gray-600 text-black flex" >
-            <input v-model="newAssignment" placeholder="New assignment..." class="" />
+            <input v-model="newAssignment" placeholder="New assignment..." class="w-full" />
             <button type="submit" class="bg-white p-1 border-l" >Add</button>
         </div>
     </form>
